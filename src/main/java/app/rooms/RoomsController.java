b822722c280514bb7d53740f37d47bdf8b4c26ee
GregBaggings <agnegerg@gmail.com>
2017-09-22 16:45:47 +0200

    Added Error and Exception handling

--- src/main/java/app/rooms/RoomsController.java
+++ src/main/java/app/rooms/RoomsController.java
@@ -1,11 +1,16 @@
 package app.rooms;
 
+import app.handlers.ErrorHandler;
 import app.models.Rooms;
 import app.models.RoomsDAO;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -13,6 +18,8 @@ import java.util.List;
  */
 @RestController
 public class RoomsController {
+    private ErrorHandler incorrectInputHandler = new ErrorHandler("Incorrect input. Please enter a valid hotel id!");
+    private ErrorHandler missingParameterHandler = new ErrorHandler("Missing param: hotelId");
 
     @Autowired
     RoomsDAO roomsDAO;
@@ -26,6 +33,22 @@ public class RoomsController {
     @RequestMapping("/v1/hotels/hotel/{hotelId}/rooms")
     public ResponseEntity<?> roomPricesForAHotel(@PathVariable("hotelId") int hotelId) {
         List<Rooms> rooms = roomsDAO.findByhotelid(hotelId);
+        if (rooms.isEmpty()) {
+            return new ResponseEntity<>(new ErrorHandler("No rooms are available for hotel with hotel id: " + hotelId + "!"), HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(rooms, HttpStatus.OK);
+
+    }
+
+    @ExceptionHandler(TypeMismatchException.class)
+    @ResponseBody
+    public ResponseEntity<?> wrongType(Exception exception, HttpServletRequest request) {
+        return new ResponseEntity<>(incorrectInputHandler, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseBody
+    public ResponseEntity<?> missingParam(Exception exception, HttpServletRequest request) {
+        return new ResponseEntity<>(missingParameterHandler, HttpStatus.BAD_REQUEST);
     }
 }
